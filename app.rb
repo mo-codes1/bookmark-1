c7ef4fffc7f42420313fe111c2f20057accb614f
Mohammed Salah <mohammed_salah@icloud.com>
2022-02-08 07:21:51 +0000

    added and passed simple feature test for viewing bookmarks

--- app.rb
+++ app.rb
@@ -10,5 +10,15 @@ class BookmarkManager < Sinatra::Base
     'Hello World'
   end
 
+  get '/bookmarks' do
+    @bookmarks = [
+              "http://www.makersacademy.com",
+              "http://www.destroyallsoftware.com",
+              "http://www.google.com"
+             ]
+  
+    erb :'bookmarks'
+  end
+
   run! if app_file == $0
 end
