2cca99975f71a0518c87a2c1f3ee6d61f5c678d6
Mohammed Salah <mohammed_salah@icloud.com>
2022-02-08 20:01:05 +0000

    refactored

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 class Bookmark
   def self.all
-    connection = PG.connect(dbname: 'bookmark_manager')
+    connection = PG.connect(dbname: 'bookmark_managerr')
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| bookmark['url'] }
   end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -7,7 +7,7 @@ describe Bookmark do
 
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com/")
+      expect(bookmarks).to include("http://www.google.com")
     end
   end
 end
